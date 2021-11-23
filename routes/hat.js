c723e09aa8e5db743724c18ed499f9a572831c4c
alap2607 <89499172+alap2607@users.noreply.github.com>
2021-11-23 15:38:41 -0500

    commit

--- routes/hat.js
+++ routes/hat.js
@@ -2,6 +2,16 @@ var express = require("express");
 const hat_controlers= require('../controllers/hat');
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on
+//or redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
+
 /* GET home page. */
 router.get('/', hat_controlers.hat_view_all_Page);
 
@@ -11,10 +21,10 @@ router.get('/detail', hat_controlers.hat_view_one_Page);
 /* GET create hat page */ 
 router.get('/create', hat_controlers.hat_create_Page); 
 
-/* GET create hat page */ 
-router.get('/update', hat_controlers.hat_update_Page);
+/* GET update hat page */ 
+router.get('/update', secured, hat_controlers.hat_update_Page);
 
-/* GET create hat page */ 
+/* GET delete hat page */ 
 router.get('/delete', hat_controlers.hat_delete_Page);
 
 
